ef3c23a335ab19c07caa12efe47882a3b456af86
Émile Jetzer <emile.jetzer@mail.mcgill.ca>
2015-11-05 22:43:29 -0500

    Added the ns_h suite.

---
+++ Summer school/ns_h/src/run_star_extras.f
@@ -0,0 +1,175 @@
+! ***********************************************************************
+!
+!   Copyright (C) 2010  Bill Paxton
+!
+!   this file is part of mesa.
+!
+!   mesa is free software; you can redistribute it and/or modify
+!   it under the terms of the gnu general library public license as published
+!   by the free software foundation; either version 2 of the license, or
+!   (at your option) any later version.
+!
+!   mesa is distributed in the hope that it will be useful, 
+!   but without any warranty; without even the implied warranty of
+!   merchantability or fitness for a particular purpose.  see the
+!   gnu library general public license for more details.
+!
+!   you should have received a copy of the gnu library general public license
+!   along with this software; if not, write to the free software
+!   foundation, inc., 59 temple place, suite 330, boston, ma 02111-1307 usa
+!
+! ***********************************************************************
+ 
+      module run_star_extras
+
+      use star_lib
+      use star_def
+      use const_def
+      
+      implicit none
+
+      double precision :: log_L_max
+      
+      integer :: time0, time1, clock_rate
+      double precision, parameter :: expected_runtime = 0.4 ! minutes
+
+      
+      contains
+      
+      subroutine extras_controls(id, ierr)
+         integer, intent(in) :: id
+         integer, intent(out) :: ierr
+         type (star_info), pointer :: s
+         ierr = 0
+         call star_ptr(id, s, ierr)
+         if (ierr /= 0) return
+      end subroutine extras_controls
+      
+      
+      integer function extras_startup(id, restart, ierr)
+         integer, intent(in) :: id
+         logical, intent(in) :: restart
+         integer, intent(out) :: ierr
+         type (star_info), pointer :: s
+         ierr = 0
+         call star_ptr(id, s, ierr)
+         if (ierr /= 0) return
+         extras_startup = 0
+         call system_clock(time0,clock_rate)
+         log_L_max = -1d99
+      end function extras_startup
+      
+
+      ! returns either keep_going, retry, backup, or terminate.
+      integer function extras_check_model(id, id_extra)
+         integer, intent(in) :: id, id_extra
+         integer :: ierr
+         type (star_info), pointer :: s
+         ierr = 0
+         call star_ptr(id, s, ierr)
+         if (ierr /= 0) return
+         extras_check_model = keep_going         
+      end function extras_check_model
+
+
+      integer function how_many_extra_history_columns(id, id_extra)
+         integer, intent(in) :: id, id_extra
+         integer :: ierr
+         type (star_info), pointer :: s
+         ierr = 0
+         call star_ptr(id, s, ierr)
+         if (ierr /= 0) return
+         how_many_extra_history_columns = 0
+      end function how_many_extra_history_columns
+      
+      
+      subroutine data_for_extra_history_columns(id, id_extra, n, names, vals, ierr)
+         integer, intent(in) :: id, id_extra, n
+         character (len=maxlen_history_column_name) :: names(n)
+         real(dp) :: vals(n)
+         integer, intent(out) :: ierr
+         type (star_info), pointer :: s
+         ierr = 0
+         call star_ptr(id, s, ierr)
+         if (ierr /= 0) return
+      end subroutine data_for_extra_history_columns
+
+      
+      integer function how_many_extra_profile_columns(id, id_extra)
+         use star_def, only: star_info
+         integer, intent(in) :: id, id_extra
+         integer :: ierr
+         type (star_info), pointer :: s
+         ierr = 0
+         call star_ptr(id, s, ierr)
+         if (ierr /= 0) return
+         how_many_extra_profile_columns = 0
+      end function how_many_extra_profile_columns
+      
+      
+      subroutine data_for_extra_profile_columns(id, id_extra, n, nz, names, vals, ierr)
+         use star_def, only: star_info, maxlen_profile_column_name
+         use const_def, only: dp
+         integer, intent(in) :: id, id_extra, n, nz
+         character (len=maxlen_profile_column_name) :: names(n)
+         real(dp) :: vals(nz,n)
+         integer, intent(out) :: ierr
+         type (star_info), pointer :: s
+         integer :: k
+         ierr = 0
+         call star_ptr(id, s, ierr)
+         if (ierr /= 0) return
+      end subroutine data_for_extra_profile_columns
+      
+
+      ! returns either keep_going, retry, backup, or terminate.
+      integer function extras_finish_step(id, id_extra)
+         use chem_def, only: ic13
+         integer, intent(in) :: id, id_extra
+         double precision, parameter :: log_L_hi = 0.9d0, log_L_lo = -0.4d0 
+         integer :: ierr
+         type (star_info), pointer :: s
+         include 'formats'
+         ierr = 0
+         call star_ptr(id, s, ierr)
+         if (ierr /= 0) return
+         extras_finish_step = keep_going
+         if (s% log_surface_luminosity > log_L_max) &
+            log_L_max = s% log_surface_luminosity
+         if (s% log_surface_luminosity > log_L_lo) return
+         if (log_L_max >= log_L_hi) then
+            write(*,'(a)') 'all values are within tolerance'
+         else
+            write(*,1) 'failed: log_L_max too small', log_L_max
+         end if
+         extras_finish_step = terminate
+      end function extras_finish_step
+      
+      
+      subroutine extras_after_evolve(id, id_extra, ierr)
+         integer, intent(in) :: id, id_extra
+         integer, intent(out) :: ierr
+         type (star_info), pointer :: s
+         double precision :: dt
+         character (len=strlen) :: test
+         ierr = 0
+         call star_ptr(id, s, ierr)
+         if (ierr /= 0) return
+         call system_clock(time1,clock_rate)
+         dt = dble(time1 - time0) / clock_rate / 60
+         call GET_ENVIRONMENT_VARIABLE( &
+            "MESA_TEST_SUITE_CHECK_RUNTIME", test, status=ierr, trim_name=.true.)
+         if (ierr == 0 .and. trim(test) == 'true' .and. dt > 1.5*expected_runtime) then
+            write(*,'(/,a70,2f12.1,99i10/)') &
+               'failed: EXCESSIVE runtime, prev time, retries, backups, steps', &
+               dt, expected_runtime, s% num_retries, s% num_backups, s% model_number
+         else
+            write(*,'(/,a50,2f12.1,99i10/)') 'runtime, prev time, retries, backups, steps', &
+               dt, expected_runtime, s% num_retries, s% num_backups, s% model_number
+         end if
+         ierr = 0
+      end subroutine extras_after_evolve
+      
+
+      end module run_star_extras
+      
